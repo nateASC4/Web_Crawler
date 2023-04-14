0a5bfa555db7e894ca9915de4c940b561166bbb1
Replit user <>
2023-04-14 15:57:26 +0000

    Struct

--- web_crawler.c
+++ web_crawler.c
@@ -14,7 +14,12 @@
 
 
 //In this program we will follow SOLID principles to ensure clarity and improve functionality of our code
-
+struct Crawled_Data{
+  char author[100];
+  char links[100];
+  char metadata[100];
+  char cookies[100];
+};
 //scan a file for URL
       // Check if filename is provided as an argument
 int step1_scanFile(){
@@ -65,17 +70,42 @@ void step0_initlibcurl(){
       // Check if the curl request was successful
   if (res != CURLE_OK) {
       printf("Error performing curl request: %s\n", curl_easy_strerror(res));
-      return 1;
+      
+  }
 
   
 }
 //this method will send request to website 
 void step2_send_request(){
+  //now that we read the file, we know which website to crawl first
+  step1_scanFile();
+  //so as we crawl we add the information to an array
+  
+  
+
+  
+
+  
+}
+///perhaps make an array a global variable
+//this method stores crawled information in an array or maybe 2D array 
+void crawled_info(){
   
 }
 
-int writeToFile(){
+void writeToFile(){
   //this will be the last function as we will write to the file after we get 
+  FILE *fp = fopen("data.txt","w");
+   if (fp == NULL) {
+        printf("Error opening file!\n");
+    }else{
+     //Write crawled information to file 
+    char arr[20][100]; 
+    for (int i =0; i < sizeof(arr); i++){
+      
+    }
+  
+
 }
   //do you want to check for sanitzation of the URL 
 //loop throug the URLs 
@@ -85,96 +115,3 @@ int currentIndex = 0;
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-size_t writeCallback(char * buffer, size_t size, size_t nmemb, void * userp) {
-  TidyBuffer * tidyBuffer = (TidyBuffer *) userp;
-  size_t newSize = size * nmemb;
-  tidyBufAppend(tidyBuffer, buffer, newSize);
-  return newSize;
-};
-
-void traverseTree(TidyNode node, char ** output) {
-  TidyNode child;
-  for (child = tidyGetChild(node); child != NULL; child = tidyGetNext(child)) {
-    TidyAttr hrefAttr = tidyAttrGetById(child, TidyAttr_HREF);
-    if (hrefAttr) {
-      if (currentIndex < MAX_LINKS) {
-        if (strlen(tidyAttrValue(hrefAttr)) < MAX_URL_LEN) {
-          strcpy(output[currentIndex], tidyAttrValue(hrefAttr));
-          currentIndex++;
-        }
-      }
-      if (tidyAttrValue(hrefAttr)) {
-        printf("Url found: %s\n", tidyAttrValue(hrefAttr));
-      }
-    }
-    traverseTree(child, output);
-  }
-}
-
-int crawlWebsite(Crawler crawler) {
-  if (crawler.url) {
-    CURL *curlHandle = curl_easy_init();
-    char errorBuffer[CURL_ERROR_SIZE];
-    int responseCode;
-    TidyDoc tidyDoc;
-    TidyBuffer tidyBuffer = {0};
-    TidyBuffer tidyErrorBuffer = {0};
-    char ** parsedUrls = (char **) malloc(MAX_LINKS * sizeof(char *));
-    for (int i = 0; i < MAX_LINKS; i++) {
-      parsedUrls[i] = (char *) malloc(MAX_URL_LEN * sizeof(char));
-    }
-    if (curlHandle) {
-      curl_easy_setopt(curlHandle, CURLOPT_URL, crawler.url);
-      curl_easy_setopt(curlHandle, CURLOPT_WRITEFUNCTION, writeCallback);
-      curl_easy_setopt(curlHandle, CURLOPT_WRITEDATA, &tidyBuffer);
-      curl_easy_setopt(curlHandle, CURLOPT_ERRORBUFFER, errorBuffer);
-      tidyDoc = tidyCreate();
-      tidyBufInit(&tidyBuffer);
-      tidyOptSetInt(tidyDoc, TidyWrapLen, 2048);
-      tidyOptSetBool(tidyDoc, TidyForceOutput, yes);
-      responseCode = curl_easy_perform(curlHandle);
-      if (responseCode == CURLE_OK) {
-        printf("Successful crawl of %s\n", crawler.url);
-        tidyParseBuffer(tidyDoc, &tidyBuffer);
-        traverseTree(tidyGetBody(tidyDoc), parsedUrls);
-        for (int i = 0; i < MAX_LINKS; i++) {
-          if (parsedUrls[i]) {
-            printf("writing %d: %s\n", i, parsedUrls[i]);
-            openFileAndWrite(OUTPUT_PATH, parsedUrls[i]);
-          }
-        }
-      } else {
-        printf("Crawl failed for %s\n", crawler.url);
-        return 0;
-      }
-      curl_easy_cleanup(curlHandle);
-      tidyBufFree(&tidyBuffer);
-      tidyBufFree(&tidyErrorBuffer);
-      tidyRelease(tidyDoc);
-      return 1;
-    }
-    return 0;
-  }
-  return 0;
-}
